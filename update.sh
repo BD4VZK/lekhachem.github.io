cd2b02f24ec64660822914b49fcddb94723ce6f1
Rachel Kam <lekha@riseup.net>
2021-04-16 23:23:13 +0800

    test

--- update.sh
+++ update.sh
@@ -1,14 +1,14 @@
-	   cat /etc/issue|sed -n '1p' > index.html;
-           uname -a|cut -d ' ' -f 2|sed s/^/HostName:' '/ >> index.html;
-           uname -a|cut -d ' ' -f 3|sed s/^/Kernel:' '/ >> index.html;
-           dmidecode -t system|awk -f /root/shells/servername.awk >> index.html;
-           echo -e '\n-------- CPU: -------' >> index.html;
-           lscpu  >> index.html;
-           echo -e '\n------ Mem: ------' >> index.html;
-           dmidecode -t memory|grep -A7 Physical >> index.html;
-           dmidecode -t memory|grep -e "Size.*[0-9]" -A8|awk -f /root/shells/mem.awk >> index.html;
-           echo -e '\n------- Disks: -------' >> index.html;
-           lsblk >> index.html;
-	   echo -e "\n\nLast updated as of $(date)." >> index.html;
-           crontab -l > crontab.html;
-           iptables -L -n > iptables.html
+	   cat /etc/issue|sed -n '1p' > index.txt;
+           uname -a|cut -d ' ' -f 2|sed s/^/HostName:' '/ >> index.txt;
+           uname -a|cut -d ' ' -f 3|sed s/^/Kernel:' '/ >> index.txt;
+           dmidecode -t system|awk -f /root/shells/servername.awk >> index.txt;
+           echo -e '\n-------- CPU: -------' >> index.txt;
+           lscpu  >> index.txt;
+           echo -e '\n------ Mem: ------' >> index.txt;
+           dmidecode -t memory|grep -A7 Physical >> index.txt;
+           dmidecode -t memory|grep -e "Size.*[0-9]" -A8|awk -f /root/shells/mem.awk >> index.txt;
+           echo -e '\n------- Disks: -------' >> index.txt;
+           lsblk >> index.txt;
+	   echo -e "\n\nLast updated as of $(date)." >> index.txt;
+           crontab -l > crontab.txt;
+           iptables -L -n > iptables.txt
