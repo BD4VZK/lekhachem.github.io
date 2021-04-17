44be558a59dcdcbd93a8ed71df17fb64f8658562
Rachel Kam <lekha@riseup.net>
2021-04-17 21:12:36 +0800

    archive

--- probe/update.sh
+++ probe/update.sh
@@ -7,6 +7,7 @@
            echo -e '\n------ Mem: ------' >> index.txt;
            dmidecode -t memory|grep -A7 Physical >> index.txt;
            dmidecode -t memory|grep -e "Size.*[0-9]" -A8|awk -f /root/shells/mem.awk >> index.txt;
+           htop >> index.txt;
            echo -e '\n------- Disks: -------' >> index.txt;
            lsblk >> index.txt;
 	   echo -e "\n\nLast updated as of $(date)." >> index.txt;
